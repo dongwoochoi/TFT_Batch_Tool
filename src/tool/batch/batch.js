36939ed8279a45ba05ff699f08232eef990ad38a
dongwoochoi <dong99woo@gmail.com>
2023-04-17 16:10:39 +0900

    batch.js state 구조 변경에 따른 이미지 적용

--- src/tool/batch/batch.js
+++ src/tool/batch/batch.js
@@ -6,7 +6,7 @@ import { input } from "../../store";
 
 function Batch() {
   let dispatch = useDispatch()
-  const [data, set_data] = useState([{0 : {}},{1 : {}},{2 : {}},{3 : {}},{4 : {}},{5 : {}},{6 : {}},{7 : {}}, ,{8 : {}}, {9 : {}},{10 : {}},{11 : {}},{12 : {}},{13 : {}},{14 : {}},{15 : {}},{16 : {}},{17 : {}},{18 : {}},{19 : {}},{20 : {}},{21 : {}},{22 : {}},{23 : {}},{24 : {}},{25 : {}},{26 : {}},{27 : {}}]);
+  const [data, set_data] = useState([{0 : {}},{1 : {}},{2 : {}},{3 : {}},{4 : {}},{5 : {}},{6 : {}},{7 : {}}, {8 : {}}, {9 : {}},{10 : {}},{11 : {}},{12 : {}},{13 : {}},{14 : {}},{15 : {}},{16 : {}},{17 : {}},{18 : {}},{19 : {}},{20 : {}},{21 : {}},{22 : {}},{23 : {}},{24 : {}},{25 : {}},{26 : {}},{27 : {}}]);
   const [drop_i, set_drop_i] = useState();
   useEffect(() => {
     dispatch(input({ i: drop_i, data: data }))
@@ -16,7 +16,7 @@ function Batch() {
   const handleDrop = (e,i) => {
     e.preventDefault();
     const copy1 = [...data]
-    copy1[i] = JSON.parse(e.dataTransfer.getData("application/json"))
+    copy1[i] = { [i] : JSON.parse(e.dataTransfer.getData("application/json"))}
     set_data(copy1);
     set_drop_i(i);
   };
@@ -25,18 +25,18 @@ function Batch() {
     const d = data[i]
     event.dataTransfer.setData("application/json", JSON.stringify(d));
     const copy2 = [...data];
-    copy2[i] = {};
+    copy2[i] = {[i] : {}};
     set_data(copy2)
   };
 
   const handleContextMenu = (event, i) => {
     event.preventDefault();
     const copy3 = [...data];
-    copy3[i] = {};
+    copy3[i] = {[i]:{}};
     set_data(copy3);
     set_drop_i(i)
   };
-
+  console.log(data)
   return (
     <div className="batch_main">
         {
@@ -46,7 +46,7 @@ function Batch() {
                         onDragStart={ (e) => handleDragStart(e, i)}
                         onDragOver={(e) => e.preventDefault()} 
                         onDrop={ (e) => handleDrop(e,i)}>
-                        <Hexagon img={data[i]}>
+                        <Hexagon img={data[i][i]}>
                             {/* <img src={data.img} alt="dropped" /> */}
                         </Hexagon>
                     </div>
